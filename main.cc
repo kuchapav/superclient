3bd52357e9ab8f5413d77f36252e9717a3fa944c
kuchapav <pavelkuchar13@seznam.cz>
2014-05-14 04:11:58 +0200

    some big changes, connection added to extra file, debuging code outputs commented ...

--- main.cc
+++ main.cc
@@ -10,126 +10,7 @@
 #include <vector>
 
 #include "message.h"
-
-
-class connection
-{
-public:
-  connection(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-  }
-
-  boost::asio::ip::tcp::socket& socket()
-  {
-    return socket_;
-  }
-
-  template <typename T, typename Handler>
-  void async_write(const T& t, Handler handler)
-  {
-    std::ostringstream archive_stream;
-    boost::archive::text_oarchive archive(archive_stream);
-    archive << t;
-    outbound_data_ = archive_stream.str();
-
-    std::ostringstream header_stream;
-    header_stream << std::setw(header_length)
-      << std::hex << outbound_data_.size();
-    if (!header_stream || header_stream.str().size() != header_length)
-    {
-      boost::system::error_code error(boost::asio::error::invalid_argument);
-      socket_.get_io_service().post(boost::bind(handler, error));
-      return;
-    }
-    outbound_header_ = header_stream.str();
-
-    std::vector<boost::asio::const_buffer> buffers;
-    buffers.push_back(boost::asio::buffer(outbound_header_));
-    buffers.push_back(boost::asio::buffer(outbound_data_));
-    boost::asio::async_write(socket_, buffers, handler);
-  }
-
-  template <typename T, typename Handler>
-  void async_read(T& t, Handler handler)
-  {
-    void (connection::*f)(
-        const boost::system::error_code&,
-        T&, boost::tuple<Handler>)
-      = &connection::handle_read_header<T, Handler>;
-    boost::asio::async_read(socket_, boost::asio::buffer(inbound_header_),
-        boost::bind(f,
-          this, boost::asio::placeholders::error, boost::ref(t),
-          boost::make_tuple(handler)));
-  }
-
-  template <typename T, typename Handler>
-  void handle_read_header(const boost::system::error_code& e,
-      T& t, boost::tuple<Handler> handler)
-  {
-    if (e)
-    {
-      boost::get<0>(handler)(e);
-    }
-    else
-    {
-      std::istringstream is(std::string(inbound_header_, header_length));
-      std::size_t inbound_data_size = 0;
-      if (!(is >> std::hex >> inbound_data_size))
-      {
-        boost::system::error_code error(boost::asio::error::invalid_argument);
-        boost::get<0>(handler)(error);
-        return;
-      }
-
-      inbound_data_.resize(inbound_data_size);
-      void (connection::*f)(
-          const boost::system::error_code&,
-          T&, boost::tuple<Handler>)
-        = &connection::handle_read_data<T, Handler>;
-      boost::asio::async_read(socket_, boost::asio::buffer(inbound_data_),
-        boost::bind(f, this,
-          boost::asio::placeholders::error, boost::ref(t), handler));
-    }
-  }
-
-  template <typename T, typename Handler>
-  void handle_read_data(const boost::system::error_code& e,
-      T& t, boost::tuple<Handler> handler)
-  {
-    if (e)
-    {
-      boost::get<0>(handler)(e);
-    }
-    else
-    {
-      try
-      {
-        std::string archive_data(&inbound_data_[0], inbound_data_.size());
-        std::istringstream archive_stream(archive_data);
-        boost::archive::text_iarchive archive(archive_stream);
-        archive >> t;
-      }
-      catch (std::exception& e)
-      {
-        boost::system::error_code error(boost::asio::error::invalid_argument);
-        boost::get<0>(handler)(error);
-        return;
-      }
-
-      boost::get<0>(handler)(e);
-    }
-  }
-
-private:
-  boost::asio::ip::tcp::socket socket_;
-  enum { header_length = 8 };
-  std::string outbound_header_;
-  std::string outbound_data_;
-
-  char inbound_header_[header_length];
-  std::vector<char> inbound_data_;
-};
+#include "connection.h"
 
 typedef boost::shared_ptr<connection> connection_ptr;
 
@@ -152,14 +33,14 @@ private:
   {
     if (!e)
     {
-      std::cout << "Povedlo se pripojit: " << conn->socket().local_endpoint() << std::endl;
+      std::cout << "Povedlo se pripojit: " << conn->socket().remote_endpoint() << std::endl;
 
       conn->async_read(data_,
           boost::bind(&superclient::handle_read, this,
             boost::asio::placeholders::error, conn));
 
-      InfoMessage msg("Vitejte!");
-      std::cout << "Vytvorena nova zprava: " << msg << std::endl;
+      Message msg("Vitejte!");
+      // std::cout << "Vytvorena nova zprava: " << msg << std::endl;
 
       conn->async_write(msg,
           boost::bind(&superclient::handle_write, this,
@@ -174,21 +55,22 @@ private:
 
   void handle_write(const boost::system::error_code& e)
   {
-    if (!e) std::cout << "povedlo se zapsat nejaka data" << std::endl;
+    // if (!e) std::cout << "povedlo se zapsat nejaka data" << std::endl;
   };
 
   void handle_read(const boost::system::error_code& e, connection_ptr conn)
   {
     if (!e)
     {
-      std::cout << "povedlo se precist nejaka data:" << data_.type() << std::endl;
+      std::cout << conn->socket().remote_endpoint() << ": " << data_ << std::endl;
+      
       conn->async_read(data_,
           boost::bind(&superclient::handle_read, this,
             boost::asio::placeholders::error, conn));
     }
     else
     {
-      std::cout << "Odpojil se: " << conn->socket().local_endpoint() << std::endl;
+      std::cout << "Odpojil se: " << conn->socket().remote_endpoint() << std::endl;
       conn->socket().close();
     }
   };
@@ -206,13 +88,13 @@ int main(int argc, char const *argv[])
     }
 
   boost::asio::io_service io_service;
-  std::cout << "io_service created" << std::endl;
+  // std::cout << "io_service created" << std::endl;
 
   boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), atoi(argv[1]));
-  std::cout << "endpoint created: " << endpoint << std::endl;
+  // std::cout << "endpoint created: " << endpoint << std::endl;
 
   superclient net(io_service, endpoint);
-  std::cout << "superclient created" << std::endl;
+  // std::cout << "superclient created" << std::endl;
 
   io_service.run();
   
--- message.h
+++ message.h
@@ -20,6 +20,18 @@ public:
   {
   };
 
+  Message(MessageType type, std::string data)
+  : type_(type),
+    data_(data)
+  {
+  };
+
+  Message(std::string data)
+  : type_(info),
+    data_(data)
+  {
+  };
+
   virtual ~Message(){}
 
   MessageType type()
@@ -27,14 +39,21 @@ public:
     return type_;
   };
 
+  std::string get_data()
+  {
+    return data_;
+  }
+
   template <typename Archive>
   void serialize(Archive& ar, const unsigned int version)
   {
     ar & type_;
+    ar & data_;
   }
 
 private:
   MessageType type_;
+  std::string data_; //zde jsou zaserializovana data (pokud nejaka takova jsou, jinak prazdny string)
 };
 
 
@@ -42,7 +61,7 @@ std::ostream & operator<<(std::ostream &os, Message& m)
 {
   switch(m.type()) {
     case 0: return os << "empty message"; break;
-    case 1: return os << "info message"; break;
+    case 1: return os << "info message - Info: " << m.get_data(); break;
     case 2: return os << "get_connections message"; break;
     case 3: return os << "pull_connection message"; break;
     case 4: return os << "reconnect message"; break;
@@ -51,68 +70,5 @@ std::ostream & operator<<(std::ostream &os, Message& m)
   }
   return os << m.type();
 }
-
-
-class InfoMessage : public Message
-{
-public:
-  InfoMessage(std::string info_str)
-  : Message(info),
-    info_(info_str)
-  {
-  };
-
-  std::string get_info()
-  {
-    return info_;
-  }
-
-  template <typename Archive>
-  void serialize(Archive& ar, const unsigned int version)
-  {
-    ar & boost::serialization::base_object<Message>(*this);
-    ar & info_;
-  }
-
-private:
-  std::string info_;
-};
-
-std::ostream & operator<<(std::ostream &os, InfoMessage& m)
-{
-  return os << "info message - Info: " << m.get_info();
-}
-
-class ConnectionMessage
-{
-public:
-  ConnectionMessage();
-  ~ConnectionMessage();
-
-private:
-    
-};
-
-
-
-class header
-{
-public:
-  header(MessageType type, int size)
-  : size_(size),
-    type_(type)
-  {
-  };
-
-  header(Message message)
-  : type_(message.type())
-  {
-  };
-
-private:
-  MessageType type_;
-  int size_;
-
-};
 
 #endif // MESSAGE_H
